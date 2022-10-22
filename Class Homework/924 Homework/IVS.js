0254eeaf363e67ce3795078d8a3836ed1d123e4b
JaiEze <jai.ezernack@outlook.com>
2022-10-22 10:18:03 -0500

    Homework Updates

---
+++ Class Homework/924 Homework/IVS.js
@@ -0,0 +1,7 @@
+myArray=[Laurence, Mike, John, Larry, Maaike];
+const document.getElementById("message");
+const document.getElementById("addFriend").addEventListener {"onclick", function() {InputEvent}};
+const document.getElementById("addNew");
+const document.getElementById("output");
+
+
