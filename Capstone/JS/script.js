1b8364626ba07342c4de611699049a184cffed18
JaiEze <jai.ezernack@outlook.com>
2022-11-03 10:42:40 -0500

    Capstone

---
+++ Capstone/JS/script.js
@@ -0,0 +1,9 @@
+// Dark & Light Mode Graduate Page 
+const open = document.getElementById('open')
+const close = document.getElementById('close')
+const container = document.querySelector('.nav-container')
+
+open.addEventListener('click', () => container.classList.add('show-nav'))
+
+close.addEventListener('click', () => container.classList.remove('show-nav'))
+
